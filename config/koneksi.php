30c695ddc285fffa532673e293f08cc051a3eadb
Hackids32 <inside.man108@gmail.com>
2023-08-04 13:33:04 +0700

    add connection

---
+++ config/koneksi.php
@@ -0,0 +1,15 @@
+<?php
+date_default_timezone_set("Asia/Jakarta");
+$db_host = "192.168.100.7";
+$db_user = "root";
+$db_pass = "myroot";
+$db_name = "hris_ntc";
+ 
+$koneksi = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+//mysqli_select_db($db_name,$koneksi);
+global $koneksi;
+ 
+if(mysqli_connect_errno()){
+	echo 'Gagal melakukan koneksi ke Database : '.mysqli_connect_error();
+}
+?>

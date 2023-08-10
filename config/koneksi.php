14837e2ca7094ca1a59ad078cc735deb24070256
Hackids32 <90852929+Hackids32@users.noreply.github.com>
2023-08-10 07:50:40 +0700

    Update koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,9 +1,9 @@
 <?php
 date_default_timezone_set("Asia/Jakarta");
-$db_host = "192.168.100.7";
-$db_user = "root";
-$db_pass = "myroot";
-$db_name = "hris_ntc";
+$db_host = "192.168.xx.xx";
+$db_user = "xx";
+$db_pass = "xx";
+$db_name = "xx";
  
 $koneksi = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 //mysqli_select_db($db_name,$koneksi);
